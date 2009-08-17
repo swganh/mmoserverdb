8e8df4f904aaa4c3b546936c9d374c60bf3bcf48
rouse <rouse@swganh.org>
2009-08-17 16:46:42 +0000

    [MySQL] - Removed chatter from moods as its a chat_type not a mood

--- Server Scripts/moods.sql
+++ Server Scripts/moods.sql
@@ -224,8 +224,7 @@ INSERT INTO `moods` (`id`,`name`) VALUES
  (184,'malevolent'),
  (185,'hurried'),
  (186,'patient'),
- (187,'firm'),
- (188,'chatter');
+ (187,'firm');
 /*!40000 ALTER TABLE `moods` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
