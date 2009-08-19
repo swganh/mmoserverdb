c010c1150fc762f4deb30bb82fdbb59defab8a4c
schmunzel <schmunzel@swganh.org>
2009-08-19 19:36:24 +0000

    implemented error channel for db/console/file logging more work on startup console logging

--- Server Scripts/config_process_list.sql
+++ Server Scripts/config_process_list.sql
@@ -1,13 +1,8 @@
-/*
----------------------------------------------------------------------------------------
-This source file is part of swgANH (Star Wars Galaxies - A New Hope - Server Emulator)
-For more information, see http://www.swganh.org
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.1.36-community
 
-
-Copyright (c) 2006 - 2009 The swgANH Team
-
----------------------------------------------------------------------------------------
-*/
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,7 +13,13 @@ Copyright (c) 2006 - 2009 The swgANH Team
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-use swganh;
+
+--
+-- Create schema swganh
+--
+
+CREATE DATABASE IF NOT EXISTS swganh;
+USE swganh;
 
 --
 -- Definition of table `config_process_list`
@@ -26,14 +27,15 @@ use swganh;
 
 DROP TABLE IF EXISTS `config_process_list`;
 CREATE TABLE `config_process_list` (
-  `id` int(10) unsigned NOT NULL default '0',
-  `address` char(16) NOT NULL default '127.0.0.1',
-  `port` int(10) unsigned NOT NULL default '0',
-  `status` int(10) unsigned NOT NULL default '0' COMMENT '0=offline, 1=loading, 2=online, 3=locked',
-  `active` int(10) unsigned NOT NULL default '0',
-  `name` char(32) NOT NULL default '',
-  `description` char(255) NOT NULL default '',
-  PRIMARY KEY  (`id`)
+  `id` int(10) unsigned NOT NULL DEFAULT '0',
+  `address` char(16) NOT NULL DEFAULT '127.0.0.1',
+  `port` int(10) unsigned NOT NULL DEFAULT '0',
+  `status` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '0=offline, 1=loading, 2=online, 3=locked',
+  `active` int(10) unsigned NOT NULL DEFAULT '0',
+  `name` char(32) NOT NULL DEFAULT '',
+  `description` char(255) NOT NULL DEFAULT '',
+  `serverstartID` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -41,60 +43,63 @@ CREATE TABLE `config_process_list` (
 --
 
 /*!40000 ALTER TABLE `config_process_list` DISABLE KEYS */;
-INSERT INTO `config_process_list` (`id`,`address`,`port`,`status`,`active`,`name`,`description`) VALUES 
- (0,'',0,0,0,'login','Handles initial connectivity and authentication of client.'),
- (1,'',56513,0,0,'connection','Handles all client connections.'),
- (2,'',0,0,0,'unused','Unused server ID.'),
- (3,'',0,0,0,'unused','Unused server ID.'),
- (4,'',0,0,0,'unused','Unused server ID.'),
- (5,'',56521,0,1,'admin','Handles miscellaneous game functions that just don\'t fit elsewhere.'),
- (6,'',1330,0,0,'chat','Handles chat,tells,mails,tickets'),
- (7,'',0,0,0,'unused','Unused server ID.'),
- (8,'',2223,0,0,'corellia','Zone server for Corellia'),
- (9,'',2133,0,0,'dantooine','Zone server for Dantooine'),
- (10,'',2169,0,0,'dathomir','Zone server for Dathomir'),
- (11,'',2139,0,0,'endor','Zone server for Endor'),
- (12,'',2151,0,0,'lok','Zone server for Lok'),
- (13,'',2127,0,0,'naboo','Zone server for Naboo'),
- (14,'',2145,0,0,'rori','Zone server for Rori'),
- (15,'',2163,0,0,'talus','Zone server for Talus'),
- (16,'',1355,0,0,'tatooine','Zone server for Tatooine'),
- (17,'',2157,0,0,'yavin4','Zone server for Yavin 4'),
- (18,'',0,0,0,'space_corellia','Zone server for Corellian Space 1'),
- (19,'',0,0,0,'space_corellia_2','Zone server for Corellian Space 2'),
- (20,'',0,0,0,'space_dantooine','Zone server for Dantooine Space'),
- (21,'',0,0,0,'space_dathomir','Zone server for Dathomir Space'),
- (22,'',0,0,0,'space_endor','Zone server for Endor Space'),
- (23,'',0,0,0,'space_env','Zone server for uknown space'),
- (24,'',0,0,0,'space_halos','Zone server for unknown space'),
- (25,'',0,0,0,'space_heavy1','Zone server for unknown space'),
- (26,'',0,0,0,'space_light1','Zone server for unknown space'),
- (27,'',0,0,0,'space_lok','Zone server for Lok Space'),
- (28,'',0,0,0,'space_naboo','Zone server for Naboo Space 1'),
- (29,'',0,0,0,'space_naboo_2','Zone server for Naboo Space 2'),
- (30,'',0,0,0,'space_tatooine','Zone server for Tatooine Space 1'),
- (31,'',0,0,0,'space_tatooine_2','Zone server for Tatooine Space 2'),
- (32,'',0,0,0,'space_yavin4','Zone server to Yavin 4 Space'),
- (33,'',1216,0,0,'09',''),
- (34,'',0,0,0,'10',''),
- (35,'',0,0,0,'11',''),
- (36,'',0,0,0,'character_farm',''),
- (37,'',0,0,0,'cinco_city_test_m5',''),
- (38,'',0,0,0,'creature_test',''),
- (39,'',0,0,0,'dungeon1',''),
- (40,'',0,0,0,'endor_asommers',''),
- (41,'',0,0,0,'floratest',''),
- (42,'',0,0,0,'godclient_test',''),
- (43,'',0,0,0,'otoh_gunga',''),
- (44,'',0,0,0,'rivertest',''),
- (45,'',0,0,0,'runtimerules',''),
- (46,'',1786,0,0,'simple',''),
- (47,'',0,0,0,'space_09',''),
- (48,'',0,0,0,'test_wearables',''),
- (49,'',56546,0,1,'tutorial',''),
- (50,'',4104,0,0,'taanab','Zone server for Tanaab'),
- (51,'',2416,0,0,'dagobah','Zone server for Dagobah.');
+INSERT INTO `config_process_list` (`id`,`address`,`port`,`status`,`active`,`name`,`description`,`serverstartID`) VALUES 
+ (0,'',0,0,0,'login','Handles initial connectivity and authentication of client.',1),
+ (1,'192.168.1.104',5000,2,0,'connection','Handles all client connections.',3),
+ (2,'',0,0,0,'unused','Unused server ID.',0),
+ (3,'',0,0,0,'unused','Unused server ID.',0),
+ (4,'',0,0,0,'unused','Unused server ID.',0),
+ (5,'192.168.1.104',5100,2,1,'admin','Handles miscellaneous game functions that just don\'t fit elsewhere.',2),
+ (6,'192.168.1.104',5300,2,0,'chat','Handles chat,tells,mails,tickets',2),
+ (7,'',0,0,0,'unused','Unused server ID.',0),
+ (8,'',2223,0,0,'corellia','Zone server for Corellia',0),
+ (9,'',2133,0,0,'dantooine','Zone server for Dantooine',0),
+ (10,'',2169,0,0,'dathomir','Zone server for Dathomir',0),
+ (11,'',2139,0,0,'endor','Zone server for Endor',0),
+ (12,'',2151,0,0,'lok','Zone server for Lok',0),
+ (13,'',2127,0,0,'naboo','Zone server for Naboo',0),
+ (14,'',2145,0,0,'rori','Zone server for Rori',0),
+ (15,'',2163,0,0,'talus','Zone server for Talus',0),
+ (16,'192.168.1.104',5200,0,0,'tatooine','Zone server for Tatooine',4),
+ (17,'',2157,0,0,'yavin4','Zone server for Yavin 4',0),
+ (18,'',0,0,0,'space_corellia','Zone server for Corellian Space 1',0),
+ (19,'',0,0,0,'space_corellia_2','Zone server for Corellian Space 2',0),
+ (20,'',0,0,0,'space_dantooine','Zone server for Dantooine Space',0),
+ (21,'',0,0,0,'space_dathomir','Zone server for Dathomir Space',0),
+ (22,'',0,0,0,'space_endor','Zone server for Endor Space',0),
+ (23,'',0,0,0,'space_env','Zone server for uknown space',0),
+ (24,'',0,0,0,'space_halos','Zone server for unknown space',0),
+ (25,'',0,0,0,'space_heavy1','Zone server for unknown space',0),
+ (26,'',0,0,0,'space_light1','Zone server for unknown space',0),
+ (27,'',0,0,0,'space_lok','Zone server for Lok Space',0),
+ (28,'',0,0,0,'space_naboo','Zone server for Naboo Space 1',0),
+ (29,'',0,0,0,'space_naboo_2','Zone server for Naboo Space 2',0),
+ (30,'',0,0,0,'space_tatooine','Zone server for Tatooine Space 1',0),
+ (31,'',0,0,0,'space_tatooine_2','Zone server for Tatooine Space 2',0),
+ (32,'',0,0,0,'space_yavin4','Zone server to Yavin 4 Space',0),
+ (33,'',1216,0,0,'09','',0),
+ (34,'',0,0,0,'10','',0),
+ (35,'',0,0,0,'11','',0),
+ (36,'',0,0,0,'character_farm','',0),
+ (37,'',0,0,0,'cinco_city_test_m5','',0),
+ (38,'',0,0,0,'creature_test','',0),
+ (39,'',0,0,0,'dungeon1','',0),
+ (40,'',0,0,0,'endor_asommers','',0),
+ (41,'',0,0,0,'floratest','',0),
+ (42,'',0,0,0,'godclient_test','',0),
+ (43,'',0,0,0,'otoh_gunga','',0),
+ (44,'',0,0,0,'rivertest','',0),
+ (45,'',0,0,0,'runtimerules','',0),
+ (46,'',1786,0,0,'simple','',0),
+ (47,'',0,0,0,'space_09','',0),
+ (48,'',0,0,0,'test_wearables','',0),
+ (49,'192.168.1.104',6000,0,0,'tutorial','',0),
+ (50,'',4104,0,0,'taanab','Zone server for Tanaab',0),
+ (51,'',2416,0,0,'dagobah','Zone server for Dagobah.',0);
 /*!40000 ALTER TABLE `config_process_list` ENABLE KEYS */;
+
+
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
