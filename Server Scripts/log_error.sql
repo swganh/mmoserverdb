45e4c28992c2937cdde7414e9ce8ab1fc4de8c17
snow <snow@swganh.org>
2009-09-07 17:40:33 +0000

    [MySQL] - Update  - fix for log spam (DatabaseError: Column 'serverstartID' cannot be NULL  - emptied the log_error table

--- Server Scripts/log_error.sql
+++ Server Scripts/log_error.sql
@@ -1,8 +1,13 @@
--- MySQL Administrator dump 1.4
---
--- ------------------------------------------------------
--- Server version	5.1.36-community
+﻿/*
+---------------------------------------------------------------------------------------
+This source file is part of swgANH (Star Wars Galaxies - A New Hope - Server Emulator)
+For more information, see http://www.swganh.org
 
+
+Copyright (c) 2006 - 2009 The swgANH Team
+
+---------------------------------------------------------------------------------------
+*/
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -13,13 +18,7 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
---
--- Create schema swganh
---
-
-CREATE DATABASE IF NOT EXISTS swganh;
-USE swganh;
+use swganh;
 
 --
 -- Definition of table `log_error`
@@ -41,19 +40,7 @@ CREATE TABLE `log_error` (
 --
 
 /*!40000 ALTER TABLE `log_error` DISABLE KEYS */;
-INSERT INTO `log_error` (`index`,`date`,`zone`,`system`,`message`,`serverstartID`) VALUES 
- (1,'2009-08-19 00:00:00','connection','ho','hu',1),
- (2,'2009-08-19 00:00:00','connection','ho','ha',1),
- (3,'2009-08-19 17:21:45','connection','ho','ha',1),
- (4,'2009-08-19 21:24:06','tatooine','Crafting','CraftingSession::selectDraftSchematic: schematic not craftable crc:1272321681',4),
- (5,'2009-08-19 21:24:16','tatooine','Crafting','CraftingSession::selectDraftSchematic: schematic not craftable crc:4228360112',4),
- (6,'2009-08-19 21:25:32','tatooine','Crafting','CraftingSession::_assembleRoll() relevant rating 143.574997',4),
- (7,'2009-08-19 21:25:32','tatooine','Crafting','CraftingSession::_assembleRoll() relevant rating modified with tool 142.074997',4),
- (8,'2009-08-19 21:25:32','tatooine','Crafting','CraftingSession::_assembleRoll() relevant Skill Mod 100',4),
- (9,'2009-08-19 21:25:32','tatooine','Crafting','CraftingSession::_assembleRoll() relevant risk -42.074997',4),
- (10,'2009-08-19 21:25:32','tatooine','Crafting','CraftingSession::_assembleRoll() relevant riskroll 11',4),
- (11,'2009-08-19 21:25:32','tatooine','Crafting','CraftingSession::_assembleRoll() relevant criticalCount 0',4),
- (12,'2009-08-19 21:25:32','tatooine','Crafting','CraftingSession::_assembleRoll() modified riskroll 11',4);
+
 /*!40000 ALTER TABLE `log_error` ENABLE KEYS */;
 
 
