091b523b5932f7e042a88b6fb86061cd1d153821
snow <snow@swganh.org>
2009-09-23 04:34:45 +0000

    [MySQL] - fix  - added anh comments so the sp fits the template

--- Server Scripts/Procedures/sp_ErrorLogHandler.sql
+++ Server Scripts/Procedures/sp_ErrorLogHandler.sql
@@ -1,10 +1,38 @@
+/*
+---------------------------------------------------------------------------------------
+This source file is part of swgANH (Star Wars Galaxies - A New Hope - Server Emulator)
+For more information, see http://www.swganh.org
+
+
+Copyright (c) 2006 - 2008 The swgANH Team
+
+---------------------------------------------------------------------------------------
+*/
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+use swganh;
+
+--
+-- Definition of procedure `sp_ErrorLogHandler`
+--
+
+DROP PROCEDURE IF EXISTS `swganh`.`sp_ErrorLogHandler`;
+
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS `swganh`.`sp_ErrorLogHandler` $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_ErrorLogHandler`(
-	 IN zone char(64),
-   IN system char(64),
-   IN message char(255))
+	IN zone char(64),
+	IN system char(64),
+	IN message char(255))
+	
 BEGIN
 
    DECLARE startindex INT;
@@ -20,4 +48,14 @@ BEGIN
 
 END $$
 
+/*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
+
 DELIMITER ;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
