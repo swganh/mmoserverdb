b7b4bae3ba35c1a1f821bcee7514b96e7bff7f42
schmunzel <schmunzel@swganh.org>
2009-10-14 08:51:12 +0000

    fixed a bug were a message could not be marked as deleteable in the session after processing thus causing the message heap to crash started implementing admin lists for structures

---
+++ Server Scripts/structure_admin_data.sql
@@ -0,0 +1,54 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.1.36-community
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema swganh
+--
+
+CREATE DATABASE IF NOT EXISTS swganh;
+USE swganh;
+
+--
+-- Definition of table `structure_admin_data`
+--
+
+DROP TABLE IF EXISTS `structure_admin_data`;
+CREATE TABLE `structure_admin_data` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `StructureID` bigint(20) unsigned DEFAULT NULL,
+  `PlayerID` bigint(20) unsigned DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `structure_admin_data`
+--
+
+/*!40000 ALTER TABLE `structure_admin_data` DISABLE KEYS */;
+INSERT INTO `structure_admin_data` (`ID`,`StructureID`,`PlayerID`) VALUES 
+ (1,2204928835602,8589934593);
+/*!40000 ALTER TABLE `structure_admin_data` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
