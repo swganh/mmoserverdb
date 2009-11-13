ef7864d2843ac5d196fc3e775a774ff23ae95631
schmunzel <schmunzel@swganh.org>
2009-11-13 23:38:11 +0000

    added a system for asynchronous checking of structure permissions - to this end the sf_checkPermissionList was added - added structure destruction as first checked command (only the owner is able to delete the structure)

---
+++ Server Scripts/Functions/sf_CheckPermissionList.sql
@@ -0,0 +1,75 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.1.36-community
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema swganh
+--
+
+CREATE DATABASE IF NOT EXISTS swganh;
+USE swganh;
+
+--
+-- Definition of function `sf_CheckPermissionList`
+--
+
+DROP FUNCTION IF EXISTS `sf_CheckPermissionList`;
+
+DELIMITER $$
+
+/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `sf_CheckPermissionList`(structure_id BIGINT(20), name CHAR(255), listname CHAR(255)) RETURNS int(11)
+BEGIN
+
+        DECLARE tmpId BIGINT(20);
+        DECLARE nameId BIGINT(20);
+        DECLARE listcount INT;
+        DECLARE ownerId BIGINT(20);
+
+        SELECT id FROM characters WHERE STRCMP(LOWER(firstname),name)=0 INTO nameId;
+
+        IF nameId IS NULL THEN RETURN(1);
+        END IF;
+
+        SELECT id FROM structure_admin_data WHERE (StructureId = structure_id and PlayerID = nameId and AdminType like listname) INTO tmpId;
+
+        IF tmpId IS NULL THEN RETURN(2);
+        END IF;
+
+        SELECT owner FROM structures WHERE structures.ID = structure_id AND owner = nameId INTO ownerId;
+
+        IF ownerId IS NOT NULL THEN RETURN(3);
+        END IF;
+
+
+
+        INSERT INTO structure_admin_data VALUES (NULL,structure_id,nameId,listname);
+
+        RETURN(0);
+
+END $$
+/*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
+
+DELIMITER ;
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
