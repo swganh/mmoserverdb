4217bb323f9a3ce48b2629b8a353f8303c0b149d
schmunzel <schmunzel@swganh.org>
2009-12-03 00:57:11 +0000

    - added default attributes to harvester deeds - added default attributes to some types of harvesters - on harvester creation the values of the deeds will be copied over to the harvester - Harvester attributes are now loaded in the harvester FactoryBase - this is the groundwork for energy and maintenance

--- Server Scripts/structure_attribute_defaults.sql
+++ Server Scripts/structure_attribute_defaults.sql
@@ -1,13 +1,8 @@
-﻿/*
----------------------------------------------------------------------------------------
-This source file is part of swgANH (Star Wars Galaxies - A New Hope - Server Emulator)
-For more information, see http://www.swganh.org
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.1.36-community
 
-
-Copyright (c) 2006 - 2009 The swgANH Team
-
----------------------------------------------------------------------------------------
-*/
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +13,12 @@ Copyright (c) 2006 - 2009 The swgANH Team
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+--
+-- Create schema swganh
+--
+
+CREATE DATABASE IF NOT EXISTS swganh;
 USE swganh;
 
 --
@@ -32,7 +33,7 @@ CREATE TABLE `structure_attribute_defaults` (
   `attribute_value` varchar(128) NOT NULL,
   `attribute_order` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `structure_attribute_defaults`
@@ -40,8 +41,19 @@ CREATE TABLE `structure_attribute_defaults` (
 
 /*!40000 ALTER TABLE `structure_attribute_defaults` DISABLE KEYS */;
 INSERT INTO `structure_attribute_defaults` (`id`,`structure_type`,`attribute_id`,`attribute_value`,`attribute_order`) VALUES 
- (1,41,17,'',1);
+ (1,41,17,'Schmunzel',1),
+ (2,51,362,'3',4),
+ (3,51,383,'30',1),
+ (4,51,382,'0',2),
+ (5,51,381,'3000',3),
+ (6,41,362,'3',4),
+ (7,41,383,'30',1),
+ (8,41,382,'0',2),
+ (9,41,381,'3000',3);
 /*!40000 ALTER TABLE `structure_attribute_defaults` ENABLE KEYS */;
+
+
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
