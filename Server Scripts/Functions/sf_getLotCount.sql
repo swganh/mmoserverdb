604467ce738cb34c71259419a13b52296ae1b69d
Schmunzel - GreatMistressoftheUniverse <schmunzel@swganh.com>
2010-04-06 23:26:50 +0200

    fixed an issue with the stored function returning NULL in case the player did not use any lots

--- Server Scripts/Functions/sf_getLotCount.sql
+++ Server Scripts/Functions/sf_getLotCount.sql
@@ -48,7 +48,11 @@ BEGIN
         DECLARE totalCount INT;
 
 
-        SELECT (SUM(st.lots_used)) FROM  structure_type_data st INNER JOIN structures ON structures.type = st.type WHERE (structures.owner = charId) INTO harvesterCount;
+        SELECT COUNT(structures.id) FROM structures WHERE (owner = charId) INTO harvesterCount;
+
+        IF(harvesterCount > 0) THEN
+          SELECT (SUM(st.lots_used)) FROM  structure_type_data st INNER JOIN structures ON structures.type = st.type WHERE (structures.owner = charId) INTO harvesterCount;
+        END IF;
 
 
         SET totalCount = (harvesterCount);
