d495940eb00c31c69a7c0d283ad72e5a60d3f752
snow <snow@swganh.org>
2010-06-15 12:21:18 -0400

    - guild table structures   + tie back to character tables

---
+++ swganh/scripts/guild_enemy.sql
@@ -0,0 +1,68 @@
+﻿/*
+---------------------------------------------------------------------------------------
+This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2010 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+use swganh;
+
+--
+-- Definition of table `guild_enemy`
+--
+
+DROP TABLE IF EXISTS `guild_enemy`;
+CREATE TABLE `guild_enemy` (
+  `id` bigint(22) unsigned NOT NULL auto_increment,
+  `guildA` bigint(22) unsigned NOT NULL,
+  `guildB` bigint(22) unsigned NOT NULL,
+  `status` int(11) unsigned NOT NULL,
+  PRIMARY KEY  USING BTREE (`id`),
+  KEY `FK_guild_enemy_types` (`status`),
+  KEY `FK_guildA` (`guildA`),
+  KEY `FK_guildB` (`guildB`),
+  CONSTRAINT `FK_guildB` FOREIGN KEY (`guildB`) REFERENCES `guilds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_guildA` FOREIGN KEY (`guildA`) REFERENCES `guilds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_guild_enemy_types` FOREIGN KEY (`status`) REFERENCES `guild_enemy_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `guild_enemy`
+--
+
+/*!40000 ALTER TABLE `guild_enemy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `guild_enemy` ENABLE KEYS */;
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
