0186e56598d54d3b4289b2efea26cb583d13d2a5
snow <snow@swganh.org>
2010-07-17 15:59:44 -0400

    Changes to nobuild stored proc (Kronos)

--- swganh/procedures/sp_PlanetNoBuildRegions.sql
+++ swganh/procedures/sp_PlanetNoBuildRegions.sql
@@ -1,8 +1,26 @@
--- MySQL Administrator dump 1.4
---
--- ------------------------------------------------------
--- Server version	5.1.47-community
+/*
+---------------------------------------------------------------------------------------
+This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
 
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2010 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -13,12 +31,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
---
--- Create schema swganh
---
-
-CREATE DATABASE IF NOT EXISTS swganh;
 USE swganh;
 
 --
@@ -32,17 +44,13 @@ DELIMITER $$
 /*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_PlanetNoBuildRegions`()
 BEGIN
-
   
-  
-  
-  
-
-  
-
-  SELECT region_id, region_name, x, z, width, height, planet_id, build, no_build_type FROM planet_regions WHERE build != 0;
-  
-  
+  ##
+  ## sp_PlanetNoBuildRegions ()
+  ##
+  ## Returns (region_id, region_name, x, z, width, height, planet, build, no_build_type)
+    
+  SELECT region_id, region_name, x, z, width, height, planet_id, build, no_build_type FROM planet_regions WHERE region_file = 'no_build_region';
 
 END $$
 /*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
