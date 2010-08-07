f64f20b2abdc5a505ebe8f7c35dacd337cca3825
snow <snow@swganh.org>
2010-08-07 00:22:12 -0400

    sp_ReturnUserAccount (cleanup / comments)

--- swganh/procedures/sp_ReturnUserAccount.sql
+++ swganh/procedures/sp_ReturnUserAccount.sql
@@ -44,11 +44,21 @@ DELIMITER $$
 /*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */ $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_ReturnUserAccount`(IN usrName CHAR(255),IN pwrd CHAR(255))
 BEGIN
-	DECLARE shaPwrd  CHAR(255);
-	SET shaPwrd = SHA1(pwrd);
-	SELECT A.account_id, A.username, A.password, A.station_id, A.banned, A.active,A.characters_allowed,A.csr 
-	FROM swganh.account A 
-	WHERE A.banned=0 AND A.authenticated=0 AND A.loggedin=0 AND A.username= usrName AND A.password = shaPwrd;	
+
+  ##
+  ## Stored Procedure
+  ##
+  ## Use: CALL sp_ReturnUserAccount(username, password);
+  ##
+  ## Returns: (server global tick)
+  
+  --
+  -- Declare Vars
+  --
+  
+DECLARE shaPwrd  CHAR(255);
+SET shaPwrd = SHA1(pwrd);
+SELECT account_id, username, password, station_id, banned, active, characters_allowed, csr FROM swganh.account WHERE banned = 0 AND authenticated = 0 AND A.loggedin=0 AND username = usrName AND password = shaPwrd;
 
 END $$
 /*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
