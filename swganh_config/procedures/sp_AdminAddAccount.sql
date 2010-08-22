e427d17c730f61e65337fe5622becd27f56bd64f
snow <snow@swganh.org>
2010-08-22 01:22:29 -0400

    fix for sp_AdminAddAccount to support new account column names

--- swganh_config/procedures/sp_AdminAddAccount.sql
+++ swganh_config/procedures/sp_AdminAddAccount.sql
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-use swganh_config;
+USE swganh_config;
 
 DELIMITER $$
 
@@ -64,7 +64,7 @@ BEGIN
       SET mAccount_id = 1;
     END IF;
 
-  SELECT MAX(station_id) + 1 FROM swganh.account INTO mStation_id;
+  SELECT MAX(account_station_id) + 1 FROM swganh.account INTO mStation_id;
 
     IF mStation_id IS NULL THEN
       SET mStation_id = 1000000;
