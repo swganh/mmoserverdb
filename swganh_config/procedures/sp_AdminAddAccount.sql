840291ff87079a9bb38eef8932ded858fe8e6279
snoow <snow@swganh.org>
2010-08-23 11:09:41 -0400

    fix for var names in sp_AdminAddAccount

--- swganh_config/procedures/sp_AdminAddAccount.sql
+++ swganh_config/procedures/sp_AdminAddAccount.sql
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-USE swganh_config;
+use swganh_config;
 
 DELIMITER $$
 
@@ -39,7 +39,7 @@ DROP PROCEDURE IF EXISTS `sp_AdminAddAccount` $$
 
 /*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='' */ $$
 
-CREATE PROCEDURE `sp_AdminAddAccount`(IN username CHAR(32), userpass CHAR(64), email CHAR(64))
+CREATE PROCEDURE `sp_AdminAddAccount`(IN musername CHAR(32), userpass CHAR(64), email CHAR(64))
 BEGIN
 
   ##
@@ -70,7 +70,7 @@ BEGIN
       SET mStation_id = 1000000;
     END IF;
 
-   INSERT INTO swganh.account VALUES (NULL, mUsername, mAccount_password, mStation_id, mAccountType, 0, mEmail, NOW(), NOW(), 1, 0, 0, 1, NULL, NOW());
+   INSERT INTO swganh.account VALUES (NULL, musername, mAccount_password, mStation_id, 0, 0, email, NOW(), NOW(), 1, 0, 0, 1, NULL);
 
   SELECT mAccount_id;
 
