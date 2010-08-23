521787d9a99798d55c50fb7fb988e6cc61f98bb0
snoow <snow@swganh.org>
2010-08-23 11:45:03 -0400

    fix

--- swganh_config/procedures/sp_AdminAddAccount.sql
+++ swganh_config/procedures/sp_AdminAddAccount.sql
@@ -70,7 +70,7 @@ BEGIN
       SET mStation_id = 1000000;
     END IF;
 
-   INSERT INTO swganh.account VALUES (NULL, musername, mAccount_password, mStation_id, 0, 0, email, NOW(), NOW(), 1, 0, 0, 1, NULL);
+   INSERT INTO swganh.account VALUES (NULL, musername, mAccount_password, mStation_id, 0, 0, email, NOW(), NOW(), 1, 0, 0, 1, NULL, NOW() - INTERVAL 1 DAY);
 
   SELECT mAccount_id;
 
