107b36c650fc8adf0d60c917ab1ab46486de71d0
snow <snow@swganh.org>
2010-08-23 23:11:15 -0400

    - removed sp_AdminAddAccount - added sp_AdminAccountAdd - added sp_AdminAccountDelete - populated name_racially_inappropriate table

--- swganh_config/procedures/sp_AdminAccountAdd.sql
+++ swganh_config/procedures/sp_AdminAccountAdd.sql
@@ -33,30 +33,43 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 USE swganh_config;
 
+--
+-- Definition of procedure `sp_AdminAccountAdd`
+--
+
+DROP PROCEDURE IF EXISTS `sp_AdminAccountAdd`;
+
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS `sp_AdminAddAccount` $$
-
-/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='' */ $$
-
-CREATE PROCEDURE `sp_AdminAddAccount`(IN username CHAR(32), userpass CHAR(64), email CHAR(64))
+/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_AdminAccountAdd`(IN username CHAR(32), userpass CHAR(64), email CHAR(64), accounttype INT(2), maxcharsallowed INT(2))
 BEGIN
+
+  ##
+  ## sp_AdminAccountAdd(username, password, email, account_type, max_characters_allowed)
+  ##
+  ## Returns
+  ##
+  ## account_id (BIGINT)
 
   ##
   ## Declare Vars
 
+
   DECLARE mAccount_id BIGINT(20);
   DECLARE mStation_id BIGINT(20);
-  DECLARE mAccount_password CHAR(64);
-
-
-  ##
-  ## Prefetch the data neeeded
-
-  SELECT SHA1(userpass) INTO mAccount_password;
+  DECLARE shapassword CHAR(64);
 
   ##
-  ## Insert the new User Account
+  ## Start
+
+  ##
+  ## Generate our password hash
+
+  SELECT SHA1(userpass) INTO shapassword;
+
+  ##
+  ## Select our next available ID
 
   SELECT MAX(account_id) + 1 FROM swganh.account INTO mAccount_id FOR UPDATE;
 
@@ -64,18 +77,26 @@ BEGIN
       SET mAccount_id = 1;
     END IF;
 
+  ##
+  ## Select our new station ID
+
   SELECT MAX(account_station_id) + 1 FROM swganh.account INTO mStation_id;
 
     IF mStation_id IS NULL THEN
       SET mStation_id = 1000000;
     END IF;
 
-   INSERT INTO swganh.account VALUES (NULL, mUsername, mAccount_password, mStation_id, mAccountType, 0, mEmail, NOW(), NOW(), 1, 0, 0, 1, NULL, NOW());
+  ##
+  ## Create the account
+
+  INSERT INTO swganh.account VALUES (NULL, username, shapassword, mStation_id, mAccountType, accounttype, mEmail, NOW(), NOW(), 1, 0, maxcharsallowed, 1, NULL, NOW() - INTERVAL 30 MINUTE);
+
+  ##
+  ## Return new account ID and Exit
 
   SELECT mAccount_id;
 
 END $$
-
 /*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
 
 DELIMITER ;
